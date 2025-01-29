ad1539fb62f875178f5e5106b727b4deee32c11f
Ronan O'Cuinn <rocuinn@gmail.com>
2025-01-29 13:06:44 +0000

    Initial commit

---
+++ src/main/java/org/ronyo/ronyo/xyz_be/Application.java
@@ -0,0 +1,13 @@
+package org.ronyo.ronyo.xyz_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
